6eb0d0baf13e492b17f34fb8253354f78bec00fb
ishit16 <ishitgarg1231@gmail.com>
2023-12-16 12:36:55 +0530

    FEATURE: Added SignUp Screen

---
+++ lib/features/auth/view/signup_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:heal_chat/constants/theme_constants.dart';
+import 'package:heal_chat/utils/widgets.dart';
+
+import '../widgets/signup_form.dart';
+import '../widgets/signup_screen_top_image.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Background(
+      child: SingleChildScrollView(
+        child: Responsive(
+          mobile: MobileSignupScreen(),
+          desktop: Row(
+            children: [
+              Expanded(
+                child: SignUpScreenTopImage(),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 450,
+                      child: SignUpForm(),
+                    ),
+                    SizedBox(height: defaultPadding / 2),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileSignupScreen extends StatelessWidget {
+  const MobileSignupScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SignUpScreenTopImage(),
+        Row(
+          children: [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: SignUpForm(),
+            ),
+            Spacer(),
+          ],
+        ),
+        // const SocalSignUp()
+      ],
+    );
+  }
+}
