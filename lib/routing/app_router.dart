403e2222cf4ac8cc0a29fb19579cc2cfd59e3294
ishit16 <ishitgarg1231@gmail.com>
2023-12-30 17:21:03 +0530

    FEATURE: AuthState Persistence Working | GoRouter Provider Created

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:heal_chat/features/auth/application/auth_service.dart';
 import 'package:heal_chat/features/auth/view/signup_screen.dart';
 import 'package:heal_chat/features/choose_volunteer/presentation/select_users_screen.dart';
 import 'package:heal_chat/features/home/presentation/home_screen.dart';
@@ -7,33 +9,52 @@ import 'package:heal_chat/features/welcome_screen/view/welcome_screen.dart';
 
 import '../features/auth/view/login_screen.dart';
 
-final goRouter = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const WelcomeScreen(),
+final goRouterProvider = Provider<GoRouter>(
+  (ref) {
+    final currentUser = ref.watch(currentUserAccountProvider);
+    print(currentUser);
+    return GoRouter(
+      initialLocation: "/",
+      redirect: (context, state) {
+        bool isLoggedIn = currentUser.value != null;
+        if (isLoggedIn) {
+          if (state.matchedLocation == "/" ||
+              state.matchedLocation == "/login") {
+            return "/home";
+          }
+        } else {
+          return "/";
+        }
+        return null;
+      },
       routes: [
         GoRoute(
-          path: 'login',
-          builder: (context, state) => const LoginScreen(),
+          path: '/',
+          builder: (context, state) => const WelcomeScreen(),
+          routes: [
+            GoRoute(
+              path: 'login',
+              builder: (context, state) => const LoginScreen(),
+            ),
+            GoRoute(
+              path: 'signup',
+              builder: (context, state) => const SignUpScreen(),
+            ),
+          ],
         ),
         GoRoute(
-          path: 'signup',
-          builder: (context, state) => const SignUpScreen(),
+          path: '/home',
+          builder: (context, state) => const HomeScreen(),
         ),
+        GoRoute(
+          path: '/users',
+          builder: (context, state) => const SelectUsersScreen(),
+        ),
+        GoRoute(
+          path: '/profile',
+          builder: (context, state) => const VolunteerProfileScreen(),
+        )
       ],
-    ),
-    GoRoute(
-      path: '/home',
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/users',
-      builder: (context, state) => const SelectUsersScreen(),
-    ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const VolunteerProfileScreen(),
-    )
-  ],
+    );
+  },
 );
