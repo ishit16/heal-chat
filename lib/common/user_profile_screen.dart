1db41416db234f91cb5bbed3a800e93a7ffbb632
ishit16 <ishitgarg1231@gmail.com>
2024-01-01 17:36:18 +0530

    REFACTOR: Migration to Common NavBar Completed

--- lib/common/user_profile_screen.dart
+++ lib/common/user_profile_screen.dart
@@ -1,8 +1,6 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:heal_chat/common/edit_user_info.dart';
-import 'package:heal_chat/common/user_profile_info.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -14,85 +12,63 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.only(top: 48),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 120,
-                      height: 120,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/images/user.jpg"),
-                      ),
+    return SingleChildScrollView(
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.only(top: 48),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset("assets/images/user.jpg"),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  "Ishit Garg",
-                  style: GoogleFonts.roboto(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
                   ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Ishit Garg",
+                style: GoogleFonts.roboto(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w400,
                 ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Edit Profile",
-                        style: GoogleFonts.lato(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600),
-                      )),
-                ),
-                const SizedBox(height: 30),
-                Divider(
-                  color: Colors.grey.shade900,
-                ),
-                const SizedBox(height: 20),
-                const EditUserInfo(),
-                // const UserProfileInformation(
-                //   schoolName: "Institute of Technology (Bombay)",
-                //   degree: 'B.Tech',
-                //   sessionsTaken: '21',
-                //   aboutVolunteerText: 'Hii',
-                // ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Edit Profile",
+                      style: GoogleFonts.lato(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    )),
+              ),
+              const SizedBox(height: 30),
+              Divider(
+                color: Colors.grey.shade900,
+              ),
+              const SizedBox(height: 20),
+              const EditUserInfo(),
+              // const UserProfileInformation(
+              //   schoolName: "Institute of Technology (Bombay)",
+              //   degree: 'B.Tech',
+              //   sessionsTaken: '21',
+              //   aboutVolunteerText: 'Hii',
+              // ),
+            ],
           ),
         ),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 65,
-        animationDuration: const Duration(milliseconds: 400),
-        backgroundColor: const Color(0xFF171821),
-        color: const Color(0xFF21222D),
-        items: const [
-          Icon(
-            Icons.home,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.person,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.settings,
-            color: Colors.white,
-          ),
-        ],
       ),
     );
   }
--- lib/features/choose_volunteer/presentation/select_users_screen.dart
+++ lib/features/choose_volunteer/presentation/select_users_screen.dart
@@ -1,4 +1,3 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:heal_chat/common/header.dart';
 import 'package:heal_chat/features/choose_volunteer/presentation/contact_card.dart';
@@ -8,43 +7,21 @@ class SelectUsersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              children: [
-                Header(
-                  heading: "Choose Volunteer",
-                  fontSize: 28,
-                ),
-                SizedBox(height: 12),
-                ContactCard(name: "Ishit Garg"),
-              ],
-            ),
+    return const SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              Header(
+                heading: "Choose Volunteer",
+                fontSize: 28,
+              ),
+              SizedBox(height: 12),
+              ContactCard(name: "Ishit Garg"),
+            ],
           ),
         ),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 65,
-        animationDuration: const Duration(milliseconds: 400),
-        backgroundColor: const Color(0xFF171821),
-        color: const Color(0xFF21222D),
-        items: const [
-          Icon(
-            Icons.home,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.person,
-            color: Colors.white,
-          ),
-          Icon(
-            Icons.settings,
-            color: Colors.white,
-          ),
-        ],
       ),
     );
   }
